0de223e27c1e1c2af8d6936a58a92bc91a8f4140
Sibel <sbaslamisli@yahoo.com>
2019-05-30 01:01:05 -0400

    updated

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "Efemir.2010",
+    "database": "Ontrack",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "Efemir.2010",
+    "database": "Ontrack",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
