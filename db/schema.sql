b226a6d33cd0830a24a31d2b1affaff648685467
Sibel <sbaslamisli@yahoo.com>
2019-05-30 14:33:00 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,64 @@
-Create database Ontrack;
-use Ontrack;
-
-CREATE TABLE IF NOT EXISTS events
-(
-  id int(11) NOT NULL auto_increment,
-  title varchar(255) NOT NULL,
-  start datetime NOT NULL,
-  end datetime NOT NULL,
-  primary key (id)
-) ;
-
+use i8qksfjkhpju15t0;
+CREATE TABLE events (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   title varchar(255) NOT NULL,
+   start varchar(255) DEFAULT NULL,
+   end varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+);
+CREATE TABLE bps (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date datetime DEFAULT NULL,
+   systolic int(11) DEFAULT NULL,
+   diastolic int(11) DEFAULT NULL,
+   pulse int(11) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
+ CREATE TABLE doctors (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   name varchar(255) DEFAULT NULL,
+   location varchar(255) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
+ CREATE TABLE exercises (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date datetime DEFAULT NULL,
+   type varchar(255) DEFAULT NULL,
+   duration int(11) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
+ CREATE TABLE foods (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date datetime DEFAULT NULL,
+   meal varchar(255) DEFAULT NULL,
+   name varchar(255) DEFAULT NULL,
+   calorie int(11) DEFAULT NULL,
+   sugar int(11) DEFAULT NULL,
+   sodium int(11) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
+ CREATE TABLE mhnotes (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date datetime DEFAULT NULL,
+   mood varchar(255) DEFAULT NULL,
+   note varchar(255) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
+ CREATE TABLE prescriptions (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   name varchar(255) DEFAULT NULL,
+   dosage varchar(255) DEFAULT NULL,
+   createdAt datetime NOT NULL,
+   updatedAt datetime NOT NULL,
+   PRIMARY KEY (id)
+ );
